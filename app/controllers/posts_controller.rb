62bce4733adb9805c8b9a4e770715a93116bed3e
Naru-hub <1208ny0324@gmail.com>
2021-07-07 08:08:00 +0000

    add post_show layout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @posts = Post.all
+    @posts = Post.order(created_at: :desc).page(params[:page]).per(15)
   end
 
   def new
