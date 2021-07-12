1c980237db40d0570714c1d944781abacd065054
Naru-hub <1208ny0324@gmail.com>
2021-07-12 05:39:13 +0000

    fix user_show_layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ApplicationRecord
   has_many :post_comments, dependent: :destroy
 
   attachment :profile_image
-
+  
+# user一覧にあるuserが飼っている犬種と大きさを検索する
   def self.search(search)
     if search != ""
       User.joins(:dogs).where("dogs.dog_size LIKE ? OR dogs.dog_type LIKE ?", "%#{search}%", "%#{search}%")
@@ -22,5 +23,33 @@ class User < ApplicationRecord
     validates :email, uniqueness: true
     validates :name
   end
+  
+  # 自分がフォローされる（被フォロー）側の関係性
+  has_many :reverse_of_relationships, 
+                          class_name: "Relationship",
+                          foreign_key: "followed_id",
+                          dependent: :destroy
+  # 自分がフォローする（与フォロー）側の関係性
+  has_many :relationships,
+                          class_name: "Relationship", 
+                          foreign_key: "follower_id",
+                          dependent: :destroy
+                          
+  # 被フォロー関係を通じて参照→自分をフォローしている人
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  
+  # 与フォロー関係を通じて参照→自分がフォローしている人
+  has_many :followings, through: :relationships, source: :followed
 
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_11_061822) do
+ActiveRecord::Schema.define(version: 2021_07_12_013530) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,14 @@ ActiveRecord::Schema.define(version: 2021_07_11_061822) do
     t.integer "dog_size", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
