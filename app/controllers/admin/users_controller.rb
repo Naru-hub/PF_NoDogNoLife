116e4115efbea491c8114d0fec145574315da8c8
Naru-hub <1208ny0324@gmail.com>
2021-07-16 09:54:34 +0000

    add admin/report part comment out

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,6 +3,7 @@ class Admin::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    # @reports = @user.reports.Post.find(post_id: post.id)
   end
 
   def update
