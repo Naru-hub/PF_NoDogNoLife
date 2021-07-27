f536c109916ddc9cb3952a9855579eb8925262e2
Naru-hub <1208ny0324@gmail.com>
2021-07-27 01:19:05 +0000

    add spec files

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションのテスト' do
+   subject { post.valid? }
+
+  let(:user) { create(:user) }
+  let!(:post) { build(:post, user_id: user.id) }
+
+  context 'placeカラム' do
+    it '空欄でないこと' do
+      post.place = ''
+      is_expected.to eq false
+    end
+  end
+
+  context 'introductionカラム' do
+      it '空欄でないこと' do
+        post.introduction = ''
+        is_expected.to eq false
+      end
+      it '1000文字以下であること: 1000文字は〇' do
+        post.introduction = Faker::Lorem.characters(number: 1000)
+        is_expected.to eq true
+      end
+      it '1000文字以下であること: 1001文字は×' do
+        post.introduction = Faker::Lorem.characters(number: 1001)
+        is_expected.to eq false
+      end
+    end
+  end
+
+  describe 'アソシエーションのテスト' do
+    context 'Userモデルとの関係' do
+      it 'N:1となっている' do
+        expect(Post.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションのテスト' do
+    subject { user.valid? }
+
+    let!(:other_user) { create(:user) }
+    let(:user) { build(:user) }
+
+    context 'nameカラム' do
+      it '空欄でないこと' do
+        user.name = ''
+        is_expected.to eq false
+      end
+      it '2文字以上であること: 1文字は×' do
+        user.name = Faker::Lorem.characters(number: 1)
+        is_expected.to eq false
+      end
+      it '2文字以上であること: 2文字は〇' do
+        user.name = Faker::Lorem.characters(number: 2)
+        is_expected.to eq true
+      end
+      it '20文字以下であること: 20文字は〇' do
+        user.name = Faker::Lorem.characters(number: 20)
+        is_expected.to eq true
+      end
+      it '20文字以下であること: 21文字は×' do
+        user.name = Faker::Lorem.characters(number: 21)
+        is_expected.to eq false
+      end
+    end
+
+    describe 'アソシエーションのテスト' do
+      context 'Postモデルとの関係' do
+        it '1:Nとなっている' do
+          expect(User.reflect_on_association(:posts).macro).to eq :has_many
+        end
+      end
+    end
+
+    describe 'アソシエーションのテスト' do
+      context 'Dogモデルとの関係' do
+        it '1:Nとなっている' do
+          expect(User.reflect_on_association(:Dogs).macro).to eq :has_many
+        end
+      end
+    end
+  end
 end
